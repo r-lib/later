d26469acf3ceba1958f3ea97a297e2aa624fcf69
Joe Cheng <joe@rstudio.com>
2017-03-20 10:30:52 -0700

    Work correctly with simple functions

--- R/later.R
+++ R/later.R
@@ -2,7 +2,31 @@
 #' @import Rcpp
 
 .onLoad <- function(...) {
-  saveNframesCallback(quote(base::sys.nframe()))
+  saveNframesCallback(quote(later:::nframe()))
+}
+
+# I don't know why, but it's necessary to wrap sys.nframe (an .Internal) inside 
+# a regular function call. Otherwise, you get the wrong results in the following
+# situation:
+#
+# a <- function() {
+#   later::later(~print("Got here 2"))
+#   Sys.sleep(1)
+#   print("Got here 1")
+# }
+# a()
+#
+# (To repro you must execute this in the R console, not source a script that 
+# contains it, as the source() call itself will ruin the repro)
+#
+# The desired behavior is to sleep 1 second, then print "Got here 1", the print 
+# "Got here 2". But if sys.nframe() is passed directly to saveNframesCallback,
+# you get "Got here 2", then sleep 1 second, then "Got here 1".
+#
+# I spent half a day with a debug version of R and gdb trying to get to the root
+# cause, without success.
+nframe <- function() {
+  sys.nframe()-1
 }
 
 #' Executes a function later
