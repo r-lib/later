2d1c57eb401d558097ad6d26124659bf0a3e54da
Joe Cheng <joe@rstudio.com>
2017-04-07 13:48:20 -0700

    Implement delay for Linux

---
+++ src/timestamp_unix.cpp
@@ -0,0 +1,56 @@
+#ifdef __unix__
+#ifndef __APPLE__ 
+
+#include "timestamp.hpp"
+#include <time.h>
+
+class TimestampImplUnix : public TimestampImpl {
+private:
+  timespec time;
+
+public:
+  TimestampImplUnix() {
+    clock_gettime(CLOCK_MONOTONIC, &this->time);
+  }
+  
+  TimestampImplUnix(double secs) {
+    clock_gettime(CLOCK_MONOTONIC, &this->time);
+    time_t wholeSecs = (long)secs;
+    long nanos = (secs - wholeSecs) * 1e9;
+    this->time.tv_sec += wholeSecs;
+    this->time.tv_nsec += nanos;
+    while (this->time.tv_nsec > 1e9) {
+      this->time.tv_sec++;
+      this->time.tv_nsec -= 1e9;
+    }
+    while (this->time.tv_nsec < 0) {
+      this->time.tv_sec--;
+      this->time.tv_nsec += 1e9;
+    }
+  }
+  
+  virtual bool future() const {
+    timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return this->time.tv_sec > now.tv_sec ||
+      (this->time.tv_sec == now.tv_sec && this->time.tv_nsec > now.tv_nsec);
+  }
+  
+  virtual bool less(const TimestampImpl* other) const {
+    const TimestampImplUnix* other_impl = dynamic_cast<const TimestampImplUnix*>(other);
+    return this->time.tv_sec < other_impl->time.tv_sec ||
+      (this->time.tv_sec == other_impl->time.tv_sec && this->time.tv_nsec < other_impl->time.tv_nsec);
+  }
+  
+  virtual bool greater(const TimestampImpl* other) const {
+    const TimestampImplUnix* other_impl = dynamic_cast<const TimestampImplUnix*>(other);
+    return this->time.tv_sec > other_impl->time.tv_sec ||
+      (this->time.tv_sec == other_impl->time.tv_sec && this->time.tv_nsec > other_impl->time.tv_nsec);
+  }
+};
+
+Timestamp::Timestamp() : p_impl(new TimestampImplUnix()) {}
+Timestamp::Timestamp(double secs) : p_impl(new TimestampImplUnix(secs)) {}
+
+#endif // __APPLE__
+#endif // __unix__
