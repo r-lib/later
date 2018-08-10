c6a963dd2d1b4ea2964d58500d8a443db12f06fe
Winston Chang <winston@stdout.org>
2018-08-10 12:03:05 -0500

    Use configure script to detect threads.h support

--- src/c11threads.h
+++ src/c11threads.h
@@ -4,10 +4,12 @@
 
 /* If there's C11 <threads.h> support, use it; otherwise use tinycthread.h,
  * which has a compatible API. */
-#ifdef THREADS_H_SUPPORT
+#if THREADS_H_SUPPORT==1
   #include <threads.h>
-#else
+#elif THREADS_H_SUPPORT==-1
   #include "tinycthread/tinycthread.h"
+#else
+  #error THREADS_H_SUPPORT must be set to 1 or -1.
 #endif
 
 
