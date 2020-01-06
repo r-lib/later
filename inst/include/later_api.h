7f4f7801d44f05c8224ed1143a7602e5beedc054
Winston Chang <winston@stdout.org>
2020-01-06 16:51:34 -0600

    Initialize execLaterNative2

--- inst/include/later_api.h
+++ inst/include/later_api.h
@@ -8,12 +8,14 @@ namespace {
   class LaterInitializer {
   public:
     LaterInitializer() {
-      // See comment in execLaterNative to learn why we need to do this
-      // in a statically initialized object
+      // See comment in execLaterNative2 to learn why we need to do this in a
+      // statically initialized object.
+      later::later(NULL, NULL, 0, GLOBAL_LOOP);
+      // For execLaterNative
       later::later(NULL, NULL, 0);
     }
   };
-  
+
   static LaterInitializer init;
 
 } // namespace
