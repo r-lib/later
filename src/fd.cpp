5985ff07f569062a105dcb4e3b3d5bddbb00e87e
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-10-18 14:29:33 +0100

    use more idiomatic c++ rather than manual memory management

--- src/fd.cpp
+++ src/fd.cpp
@@ -7,37 +7,38 @@
 #include "later.h"
 #include "debug.h"
 
-typedef struct thread_args_s {
+typedef struct ThreadArgs_s {
   SEXP callback;
   R_xlen_t num_fds;
-  int *fds;
+  std::unique_ptr<std::vector<int>> fds;
   fd_set read_fds;
   struct timeval tv;
   int max_fd;
   int loop;
   bool timer_inf;
-} thread_args;
+} ThreadArgs;
 
 static void later_callback(void *arg) {
 
-  thread_args *args = (thread_args *) arg;
+  std::unique_ptr<std::shared_ptr<ThreadArgs>> argsptr(static_cast<std::shared_ptr<ThreadArgs>*>(arg));
+  std::shared_ptr<ThreadArgs> args = *argsptr;
+
   SEXP results, call;
   PROTECT(results = Rf_allocVector(LGLSXP, args->num_fds));
   int *res = INTEGER(results);
-  memcpy(res, args->fds, args->num_fds * sizeof(int));
+  std::memcpy(res, args->fds->data(), args->num_fds * sizeof(int));
   PROTECT(call = Rf_lcons(args->callback, Rf_cons(results, R_NilValue)));
   Rf_eval(call, R_GlobalEnv);
   UNPROTECT(2);
   R_ReleaseObject(args->callback);
-  R_Free(args->fds);
-  R_Free(args);
 
 }
 
 // TODO: add method for HANDLES on Windows
 static void *select_thread(void *arg) {
 
-  thread_args *args = (thread_args *) arg;
+  std::unique_ptr<std::shared_ptr<ThreadArgs>> argsptr(static_cast<std::shared_ptr<ThreadArgs>*>(arg));
+  std::shared_ptr<ThreadArgs> args = *argsptr;
 
   int ready = select(args->max_fd + 1, &args->read_fds, NULL, NULL, args->timer_inf ? NULL : &args->tv);
 
@@ -45,18 +46,16 @@ static void *select_thread(void *arg) {
     // TODO: fix errno on Windows
     err_printf("select error: %s\n", strerror(errno));
     R_ReleaseObject(args->callback);
-    R_Free(args->fds);
-    R_Free(args);
-    return NULL;
+    return nullptr;
   }
 
   for (R_xlen_t i = 0; i < args->num_fds; i++) {
-    args->fds[i] = FD_ISSET(args->fds[i], &args->read_fds) != 0;
+    (*args->fds)[i] = FD_ISSET((*args->fds)[i], &args->read_fds) != 0;
   }
 
-  execLaterNative2(later_callback, arg, 0, args->loop);
+  execLaterNative2(later_callback, static_cast<void *>(argsptr.release()), 0, args->loop);
 
-  return NULL;
+  return nullptr;
 
 }
 
@@ -74,9 +73,8 @@ Rcpp::LogicalVector execLater_fd(Rcpp::Function callback, Rcpp::IntegerVector fd
   int loop = loop_id[0];
   int max_fd = -1;
 
-  thread_args *args = R_Calloc(1, thread_args);
-  int *fdvals = R_Calloc(num_fds, int);
-  args->fds = fdvals;
+  std::shared_ptr<ThreadArgs> args = std::make_shared<ThreadArgs>();
+  auto fdvals = std::make_unique<std::vector<int>>(num_fds);
   args->num_fds = num_fds;
   R_PreserveObject(callback);
   args->callback = callback;
@@ -85,11 +83,12 @@ Rcpp::LogicalVector execLater_fd(Rcpp::Function callback, Rcpp::IntegerVector fd
   FD_ZERO(&args->read_fds);
 
   if (num_fds)
-    memcpy(fdvals, fds.begin(), num_fds * sizeof(int));
+    std::memcpy(fdvals->data(), fds.begin(), num_fds * sizeof(int));
   for (R_xlen_t i = 0; i < num_fds; i++) {
-    FD_SET(fdvals[i], &args->read_fds);
-    max_fd = std::max(max_fd, fdvals[i]);
+    FD_SET((*fdvals)[i], &args->read_fds);
+    max_fd = std::max(max_fd, (*fdvals)[i]);
   }
+  args->fds = std::move(fdvals);
   args->max_fd = max_fd;
 
   if (timeoutSecs[0] == R_PosInf) {
@@ -99,9 +98,11 @@ Rcpp::LogicalVector execLater_fd(Rcpp::Function callback, Rcpp::IntegerVector fd
     args->tv.tv_usec = ((int) (timeoutSecs[0] * 1000)) % 1000 * 1000;
   }
 
+  auto argsptr = std::make_unique<std::shared_ptr<ThreadArgs>>(args);
+
 #ifdef _WIN32
 
-  HANDLE hThread = CreateThread(NULL, 0, select_thread_win, (LPVOID) args, 0, NULL);
+  HANDLE hThread = CreateThread(NULL, 0, select_thread_win, static_cast<LPVOID>(argsptr.release()), 0, NULL);
 
   if (hThread == NULL) {
     Rcpp::stop("thread creation error: " + std::to_string(GetLastError()));
@@ -118,7 +119,7 @@ Rcpp::LogicalVector execLater_fd(Rcpp::Function callback, Rcpp::IntegerVector fd
     Rcpp::stop("pthread error: " + std::string(strerror(errno)));
 
   if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) ||
-      pthread_create(&t, &attr, select_thread, (void *) args)) {
+      pthread_create(&t, &attr, select_thread, static_cast<void *>(argsptr.release()))) {
     pthread_attr_destroy(&attr);
     Rcpp::stop("pthread error: " + std::string(strerror(errno)));
   }
