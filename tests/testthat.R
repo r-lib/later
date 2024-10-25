520cbfc9b95018c525c9bf3eff05b78b22263fa8
Joe Cheng <joe@posit.co>
2024-10-25 02:41:07 -0700

    Try to get better testthat logs

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,4 +4,19 @@ Sys.setenv("R_TESTS" = "")
 library(testthat)
 library(later)
 
-test_check("later")
+DetailedSummaryReporter <- R6::R6Class("DetailedSummaryReporter", inherit = testthat::SummaryReporter,
+  public = list(
+    start_test = function(context, test) {
+      self$cat_tight("    ", test, ": ")
+    },
+    end_test = function(context, test) {
+      self$cat_line()
+    },
+    start_context = function(context) {
+      self$cat_tight(context, ":\n")
+    },
+    end_context = function(context) { }
+  )
+)
+
+test_check("later", reporter = DetailedSummaryReporter)
