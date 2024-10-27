3501915798e4de8bbc4b91f019fbf7e82604b7be
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-10-27 15:32:43 +0000

    add more tests

--- tests/testthat/test-later-fd.R
+++ tests/testthat/test-later-fd.R
@@ -5,18 +5,22 @@ test_that("later_fd", {
 
   result <- NULL
   callback <- function(x) result <<- x
-  s1 <- nanonext::socket(listen = "inproc://nano")
-  s2 <- nanonext::socket(dial = "inproc://nano")
+  s1 <- nanonext::socket(listen = "inproc://nanonext")
+  s2 <- nanonext::socket(dial = "inproc://nanonext")
   fd1 <- nanonext::opt(s1, "recv-fd")
   fd2 <- nanonext::opt(s2, "recv-fd")
 
-  # 1. timeout
+  # timeout
   later_fd(callback, c(fd1, fd2), timeout = 0)
   Sys.sleep(0.2)
   run_now()
   expect_equal(result, c(FALSE, FALSE))
+  later_fd(callback, c(fd1, fd2), exceptfds = c(fd1, fd2), timeout = 0)
+  Sys.sleep(0.2)
+  run_now()
+  expect_equal(result, c(FALSE, FALSE, FALSE, FALSE))
 
-  # 2. cancellation
+  # cancellation
   result <- NULL
   cancel <- later_fd(callback, c(fd1, fd2), timeout = 0.2)
   expect_type(cancel, "closure")
@@ -27,43 +31,55 @@ test_that("later_fd", {
   run_now()
   expect_null(result)
 
-  # 3. fd1 active
+  # timeout (> 1 loop)
+  later_fd(callback, c(fd1, fd2), timeout = 1.1)
+  Sys.sleep(1.25)
+  run_now()
+  expect_equal(result, c(FALSE, FALSE))
+
+  # fd1 ready
   later_fd(callback, c(fd1, fd2), timeout = 0.9)
   res <- nanonext::send(s2, "msg")
   Sys.sleep(0.2)
   run_now()
   expect_equal(result, c(TRUE, FALSE))
 
-  # 4. both active
+  # both fd1, fd2 ready
   res <- nanonext::send(s1, "msg")
   later_fd(callback, c(fd1, fd2), timeout = 1)
   Sys.sleep(0.2)
   run_now()
   expect_equal(result, c(TRUE, TRUE))
 
-  # 5. fd2 active
+  # no exceptions
+  later_fd(callback, c(fd1, fd2), exceptfds = c(fd1, fd2), timeout = -0.1)
+  Sys.sleep(0.2)
+  run_now()
+  expect_equal(result, c(TRUE, TRUE, FALSE, FALSE))
+
+  # fd2 ready
   res <- nanonext::recv(s1)
   later_fd(callback, c(fd1, fd2), timeout = 1L)
   Sys.sleep(0.2)
   run_now()
   expect_equal(result, c(FALSE, TRUE))
 
-  # 6. fd2 invalid - returns NA for fd2
+  # fd2 invalid
+  res <- nanonext::recv(s2)
+  later_fd(callback, c(fd1, fd2), exceptfds = c(fd1, fd2), timeout = 0.2)
   close(s2)
-  later_fd(callback, c(fd1, fd2), timeout = 1)
   Sys.sleep(0.2)
   run_now()
-  if (.Platform$OS.type != "windows")
-    expect_equal(result, c(FALSE, NA))
+  expect_length(result, 4L)
 
-  # 7. both fds invalid - returns all NA
+  # both fd1, fd2 invalid
   close(s1)
-  later_fd(callback, c(fd1, fd2), timeout = 1)
+  later_fd(callback, c(fd1, fd2), c(fd1, fd2), timeout = -1L)
   Sys.sleep(0.2)
   run_now()
-  expect_equal(result, c(NA, NA))
+  expect_equal(result, c(NA, NA, NA, NA))
 
-  # 8. errors
+  # errors
   expect_error(later_fd(print), "No file descriptors supplied")
 
 })
