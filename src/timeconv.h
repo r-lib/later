8ff0f4a5c2d562a1c933d08202a42f013919edd9
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-04-01 09:31:44 +0100

    Eliminate implicit type conversions

--- src/timeconv.h
+++ src/timeconv.h
@@ -18,20 +18,20 @@ inline timespec timevalToTimespec(const timeval& tv) {
 inline timeval timespecToTimeval(const timespec& ts) {
   timeval tv;
   tv.tv_sec = ts.tv_sec;
-  tv.tv_usec = ts.tv_nsec / 1000;
+  tv.tv_usec = (suseconds_t) (ts.tv_nsec / 1000);
   return tv;
 }
 
 inline timespec addSeconds(const timespec& time, double secs) {
   timespec ts = time;
   ts.tv_sec += (time_t)secs;
-  ts.tv_nsec += (secs - (time_t)secs) * 1e9;
+  ts.tv_nsec += (secs - (time_t)secs) * 1e9L;
   if (ts.tv_nsec < 0) {
-    ts.tv_nsec += 1e9;
+    ts.tv_nsec += 1e9L;
     ts.tv_sec--;
   }
-  if (ts.tv_nsec >= 1e9) {
-    ts.tv_nsec -= 1e9;
+  if (ts.tv_nsec >= 1e9L) {
+    ts.tv_nsec -= 1e9L;
     ts.tv_sec++;
   }
   return ts;
