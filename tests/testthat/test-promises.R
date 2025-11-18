9f89ecd18a16ff49148cf93f7d492a50d2a16abd
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-11-18 22:35:52 +0000

    Simplify promises test

--- tests/testthat/test-promises.R
+++ tests/testthat/test-promises.R
@@ -1,61 +1,8 @@
-test_that("later C++ interface works with promises", {
+test_that("later C++ BackgroundTask class works with promises", {
 
   # This test was originally in the promises package but moved here
   # to consolidate everything that requires compilation
   skip_if_not_installed("promises")
-
-  # test helper
-  create_counting_domain <- function() {
-    counts <- list2env(
-      parent = emptyenv(),
-      list(
-        onFulfilledBound = 0L,
-        onFulfilledCalled = 0L,
-        onFulfilledActive = 0L,
-        onRejectedBound = 0L,
-        onRejectedCalled = 0L,
-        onRejectedActive = 0L
-      )
-    )
-
-    incr <- function(field) {
-      field <- as.character(substitute(field))
-      counts[[field]] <- counts[[field]] + 1L
-    }
-
-    decr <- function(field) {
-      field <- as.character(substitute(field))
-      counts[[field]] <- counts[[field]] - 1L
-    }
-
-    promises::new_promise_domain(
-      wrapOnFulfilled = function(onFulfilled) {
-        incr(onFulfilledBound)
-        function(...) {
-          incr(onFulfilledCalled)
-          incr(onFulfilledActive)
-          on.exit(decr(onFulfilledActive))
-
-          onFulfilled(...)
-        }
-      },
-      wrapOnRejected = function(onRejected) {
-        incr(onRejectedBound)
-        function(...) {
-          incr(onRejectedCalled)
-          incr(onRejectedActive)
-          on.exit(decr(onRejectedActive))
-
-          onRejected(...)
-        }
-      },
-      counts = counts
-    )
-  }
-
-  # grab promises function
-  ns <- getNamespace("promises")
-  current_promise_domain <- ns$current_promise_domain
 
   # compile test
   env <- new.env()
@@ -71,29 +18,9 @@ test_that("later C++ interface works with promises", {
     promises::then(\(x) {
       expect_identical(x, 2)
     })
+  run_now(1)
   while (!loop_empty()) {
-    run_now(1)
+    run_now(0.1)
   }
 
-  # test works with promise domains
-  cd <- create_counting_domain()
-
-  expect_true(is.null(current_promise_domain()))
-  promises::with_promise_domain(cd, {
-    promises::promise(function(resolve, reject) {
-      env$asyncFib(resolve, reject, 3)
-    }) |>
-      promises::then(\(x) {
-        expect_identical(x, 2)
-        expect_identical(cd$counts$onFulfilledCalled, 1L)
-        promises::promise_resolve(TRUE) |>
-          promises::then(\(y) {
-            expect_true(!is.null(current_promise_domain()))
-            expect_identical(cd$counts$onFulfilledCalled, 2L)
-          })
-      })
-    while (!loop_empty()) {
-      run_now(1)
-    }
-  })
 })
