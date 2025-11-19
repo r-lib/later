b2ea7657bd66c37c1cc1969fa8ebf5ee35b6917e
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2025-11-19 15:47:17 +0000

    Apply suggestions from code review
    Co-authored-by: Barret Schloerke <schloerke@gmail.com>

--- tests/testthat/test-promises.R
+++ tests/testthat/test-promises.R
@@ -11,16 +11,34 @@ test_that("later C++ BackgroundTask class works with promises", {
     env = env
   )
 
-  # test that it works
+  # test that resolve works
+  result <- 0
   promises::promise(function(resolve, reject) {
     env$asyncFib(resolve, reject, 3)
   }) |>
     promises::then(\(x) {
-      expect_identical(x, 2)
+      result <<- x
     })
+
+  expect_identical(result, 0)
   run_now(1)
   while (!loop_empty()) {
     run_now(0.1)
   }
+  expect_identical(result, 2)
+  # test that reject works (swap resolve/reject)
+  err_result <- 0
+  promises::promise(function(resolve, reject) {
+    env$asyncFib(reject, resolve, 6)
+  }) |>
+    promises::catch(\(x) {
+      err_result <<- x
+    })
 
+  expect_identical(err_result, 0)
+  run_now(1)
+  while (!loop_empty()) {
+    run_now(0.1)
+  }
+  expect_identical(err_result, 8)
 })
